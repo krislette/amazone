bb50ca98d220609770e47cb9cfdc5588aa20959a
acellle <acellekrislette@gmail.com>
2024-08-06 17:06:21 +0800

    Delete products from cart when clicking del

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
 }, {
@@ -23,4 +23,16 @@ export function addToCart(productId) {
             quantity: 1
         });
     }
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem); 
+        }
+    });
+
+    cart = newCart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -27,13 +27,13 @@ cart.forEach((cartItem) => {
                 </div>
                 <div class="product-quantity">
                     <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                        Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                     </span>
                     <span class="update-quantity-link link-primary">
-                    Update
+                        Update
                     </span>
-                    <span class="delete-quantity-link link-primary">
-                    Delete
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                        Delete
                     </span>
                 </div>
                 </div>
@@ -88,3 +88,10 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
+
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+    link.addEventListener("click", () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+    });
+});
