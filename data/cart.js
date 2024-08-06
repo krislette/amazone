d8d7d24ecb48269dc00c656da21448a9e48cdca8
acellle <acellekrislette@gmail.com>
2024-08-06 16:45:35 +0800

    Added dummy on cart, generated html on checkout

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+}, {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}];
 
 export function addToCart(productId) {
     let matchingItem;
