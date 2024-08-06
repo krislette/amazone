23f58b3ec7e691a30eac1a616bf84c46c254aac5
acellle <acellekrislette@gmail.com>
2024-08-06 17:10:22 +0800

    Delete products from html when clicking del

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -9,7 +9,7 @@ cart.forEach((cartItem) => {
     let matchingProduct = products.find(({ id }) => id === cartItem.productId);
 
     cartSummaryHTML+=  `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -93,5 +93,7 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
     link.addEventListener("click", () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove(); // Fetch element from html and remove it using .remove()
     });
 });
