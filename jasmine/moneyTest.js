93792aaed9d7fec2f3d698953b437bea5a0ac6e0
acellle <acellekrislette@gmail.com>
2024-08-08 15:29:26 +0800

    Recreated tests with jasmine, removed default tests

---
+++ jasmine/moneyTest.js
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+// Provided by jasmine, and this creates a test suite
+describe("Test suite: formatCurrency", () => {
+    // Provided by jasmine, and this craetes a test
+    it("Converts cents into dollars", () => {
+        // Code what the test is
+        // Expect let's you compare a value to another value
+        expect(formatCurrency(2095)).toEqual("20.95");
+    });
+
+    it("Works with 0", () => {
+        expect(formatCurrency(0)).toEqual("0.00");
+    });
+
+    it("Rounds up to the nearest cent", () => {
+        expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
+});
