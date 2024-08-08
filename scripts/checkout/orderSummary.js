5a8e19e2b8fab6dd660da0632b826ded0f72246c
acellle <acellekrislette@gmail.com>
2024-08-09 03:08:37 +0800

    Solutions for exercises g and h

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -28,11 +28,13 @@ export function renderOrderSummary() {
                         src="${matchingProduct.image}">
 
                     <div class="cart-item-details">
-                        <div class="product-name">
+                        <div class="product-name 
+                            js-product-name-${matchingProduct.id}">
                             ${matchingProduct.name}
                         </div>
-                        <div class="product-price">
-                            ${formatCurrency(matchingProduct.priceCents)}
+                        <div class="product-price
+                            js-product-price-${matchingProduct.id}">
+                            $${formatCurrency(matchingProduct.priceCents)}
                         </div>
                         <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                             <span>
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -59,7 +59,25 @@ describe("Test suite: renderOrderSummary", () => {
  
         expect(
             document.querySelector(`.js-product-quantity-${productId2}`).innerText
-        ).toContain("Quantity: 1"); 
+        ).toContain("Quantity: 1");
+
+        // Exercise: Expects that the page correctly displays the name
+        expect(
+            document.querySelector(`.js-product-name-${productId1}`).innerText
+        ).toEqual("Black and Gray Athletic Cotton Socks - 6 Pairs");
+
+        expect(
+            document.querySelector(`.js-product-name-${productId2}`).innerText
+        ).toEqual("Intermediate Size Basketball");
+
+        // Exercise: Expects that the page correctly displays the price
+        expect(
+            document.querySelector(`.js-product-price-${productId1}`).innerText
+        ).toEqual("$10.90");
+
+        expect(
+            document.querySelector(`.js-product-price-${productId2}`).innerText
+        ).toEqual("$20.95");
     });
     
     it("Removes a product", () => {
@@ -80,6 +98,14 @@ describe("Test suite: renderOrderSummary", () => {
         expect(
             document.querySelector(`.js-cart-item-container-${productId2}`)
         ).not.toEqual(null);
+
+        expect(
+            document.querySelector(`.js-product-name-${productId2}`).innerText
+        ).toEqual("Intermediate Size Basketball");
+
+        expect(
+            document.querySelector(`.js-product-price-${productId2}`).innerText
+        ).toEqual("$20.95");
         
         expect(cart.length).toEqual(1);
         expect(cart[0].productId).toEqual(productId2);
