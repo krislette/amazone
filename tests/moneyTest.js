d91554f5ee89eff8a341a236f47f5a1fd0e145eb
acellle <acellekrislette@gmail.com>
2024-08-08 13:52:51 +0800

    Note comment on lines 19 to 20

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -9,10 +9,12 @@ console.log(formatCurrency(2095) === "20.95" ? "Passed" : "Failed");
 
 // Test case 2
 console.log("Works with 0:");
-console.log(formatCurrency(0) === "0.00" ? "Passed" : "Failed"); 
+console.log(formatCurrency(0) === "0.00" ? "Passed" : "Failed");
 
 // Test case 3
 console.log("Rounds up to the nearest cent:");
-console.log(formatCurrency(2000.5) === "20.01" ? "Passed" : "Failed"); 
+console.log(formatCurrency(2000.5) === "20.01" ? "Passed" : "Failed");
 
 // Group of related tests = test suite
+// Aumated testing like this is still long, so a
+// `Testing Framework` can be used - Jasmine framework
